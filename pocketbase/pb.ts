0e9cc3a51afdf92ba782b1d0e1852b6523dc1100
Lacroix-Eva <eva.lacroix@edu.univ-fcomte.fr>
2025-11-14 14:32:03 +0100

    on test le vps

--- pocketbase/pb.ts
+++ pocketbase/pb.ts
@@ -1,3 +1,8 @@
 import PocketBase from 'pocketbase';
-
-export const pb = new PocketBase('http://127.0.0.1:8090'); // URL de ton PocketBase
+import type { TypedPocketBase } from "./pocketbase-types";
+var path='';
+if(import.meta.env.MODE === 'development')
+    path = 'http://localhost:8090'    //localhost = machine de dev
+else path = 'https://portfolio.eva-lacroix.fr:443'   //url du site 
+const pb = new PocketBase(path) as TypedPocketBase;
+export default pb;
